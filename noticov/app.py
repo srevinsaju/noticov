a30644779865a88234556ec5ef11768ade05853f
Srevin Saju <srevinsaju@sugarlabs.org>
2021-05-09 00:32:09 +0300

    feat: rearrange the backend process

--- noticov/app.py
+++ noticov/app.py
@@ -1,28 +1,42 @@
 
 import os
 from dotenv import load_dotenv
-import
 
-
-
-
+from noticov.backend.base import BaseConnection
+from noticov.backend.postgresql import PostgreSQLConnection
+from noticov.exceptions import DBStringNotFound
 
 
 class NotiCovBackend:
-    def __init__(self):
+    def __init__(self,
+                 connection: BaseConnection = None):
+        if connection is None:
+            raise ConnectionError("BaseConnection failed to initialize..")
+
+    def loop(self):
+        """
+        Something which will be run
+        :return:
+        :rtype:
+        """
         pass
 
     def start(self):
-
         pass
 
 
 def main():
-    ncb = NotiCovBackend()
     load_dotenv()
 
     db_string = os.getenv("DB_STRING")
+    if not db_string:
+        raise DBStringNotFound("$DB_STRING is not defined in the environment. Please define it with"
+                               "DB_STRING=\"...\" python3 -m noticov to run this software")
 
+    # initialize the psql connection
+    psql = PostgreSQLConnection(destination=db_string)
+
+    ncb = NotiCovBackend(connection=psql)
 
 
 
