b4be7346875f87e15074ed1835c33c43d76576e8
Srevin Saju <srevinsaju@sugarlabs.org>
2021-05-08 23:37:51 +0300

    feat: move to sqlalchemy for the backend sql magic

--- noticov/backend/postgresql.py
+++ noticov/backend/postgresql.py
@@ -1,9 +1,12 @@
-from typing import List
+import uuid
+from typing import List, Dict
 
-import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, String, MetaData, Integer
+from sqlalchemy.sql import FromClause
 
 from noticov.backend.base import BaseConnection
-from noticov.backend.tables import Table
+from noticov.backend.tables import Tables
 from noticov.covidstats.data import CovidData, CovidDataAttr
 from noticov.logging import make_logger
 
@@ -14,49 +17,66 @@ class PostgreSQLConnection(BaseConnection):
 
     def __init__(self, destination: str = None):
         super().__init__()
-        self.conn = psycopg2.connect(destination)
+        self.db = create_engine(destination)
+        self.conn = self.db.connect()
+        self.meta = MetaData(self.db)
+        self.tables: Dict[Tables, Table] = dict()
+
         self.logger.info("Connection to postgresql database successful")
         self.initialize()
 
     def initialize(self):
-        with self.conn.cursor() as cur:
-            for table in Table:
-                cur.execute(
-                    f"CREATE TABLE IF NOT EXISTS {table} ("
-                    f"{CovidDataAttr.ID} INT PRIMARY KEY, "
-                    f"{CovidDataAttr.TIMESTAMP} INT, "
-                    f"{CovidDataAttr.DEATHS} INT, "
-                    f"{CovidDataAttr.TOTAL_CASES} INT, "
-                    f"{CovidDataAttr.DISCHARGED} INT, "
-                    f"{CovidDataAttr.LOCATION} VARCHAR(50), "
-                    f")"
-                )
+        for table in Tables:
+            self.tables[table] = _table = \
+                Table(Tables.INDIA, self.meta,
+                      Column(CovidDataAttr.ID, String),
+                      Column(CovidDataAttr.LOCATION, String),
+                      Column(CovidDataAttr.TOTAL_CASES, Integer),
+                      Column(CovidDataAttr.DISCHARGED, Integer),
+                      Column(CovidDataAttr.DEATHS, Integer),
+                      Column(CovidDataAttr.TIMESTAMP, Integer),
+                      )
 
-    def add_data(self, data: CovidData):
-        with self.conn.cursor() as cur:
-            for table in Table:
-                cur.execute(
-                    f"CREATE TABLE IF NOT EXISTS {table} ("
-                    f"{CovidDataAttr.ID} INT PRIMARY KEY, "
-                    f"{CovidDataAttr.TIMESTAMP} INT, "
-                    f"{CovidDataAttr.DEATHS} INT, "
-                    f"{CovidDataAttr.TOTAL_CASES} INT, "
-                    f"{CovidDataAttr.DISCHARGED} INT, "
-                    f"{CovidDataAttr.LOCATION} VARCHAR(50), "
-                    f")"
-                )
+            _table.create()
 
-    def add_multiple_data(self, data_sequence: List[CovidData]):
+    def add_data(self, data: CovidData, table: Tables):
+        self.conn.execute(
+            self.tables[table].insert().values(
+                ((CovidDataAttr.ID, uuid.uuid4()),
+                 (CovidDataAttr.TIMESTAMP, data.timestamp),
+                 (CovidDataAttr.DEATHS, data.deaths),
+                 (CovidDataAttr.TOTAL_CASES, data.total_cases),
+                 (CovidDataAttr.DISCHARGED, data.discharged),
+                 (CovidDataAttr.LOCATION, data.location),
+                 )
+            )
+        )
+        self.conn.commit()
+
+    def add_multiple_data(self, data_sequence: List[CovidData], table: Tables):
         # TODO: Improve the performance by doing only a single request
         for data in data_sequence:
-            self.add_data(data)
+            self.add_data(data, table)
 
-    def get_all_covid_data(self, table: Table, location: str):
-        with self.conn.cursor() as cur:
-            # Check the current balance.
-            cur.execute(f"SELECT "
-                        f"{CovidDataAttr.TOTAL_CASES}, "
-                        f"{CovidDataAttr.DEATHS}, "
-                        f"{CovidDataAttr.DISCHARGED} FROM {table} "
-                        f"WHERE {CovidDataAttr.LOCATION} = \"{location}\"")
+    def get_all_covid_data(self, table: Tables, location: str):
+        # TODO: looks sus.. pls fix someone 🥺🥺🥺
+        where_expression = self.tables[table].c.loc == location
 
+        resultset = self.conn.execute(self.tables[table].select().where(where_expression))
+        self.logger.debug(f"Received {len(resultset)} rows from {self.connection}")
+        # TODO: fix this part, is messed up again
+        return resultset
+
+    def get_latest_covid_data(self, table: Tables, location: str):
+        # TODO: looks sus.. pls fix someone 🥺🥺🥺
+        where_expression = self.tables[table].c.loc == location
+
+        resultset = self.conn.execute(self.tables[table]
+                                      .select()
+                                      .where(where_expression)
+                                      .order_by(CovidDataAttr.TIMESTAMP)
+                                      .limit(1)
+                                      )
+        self.logger.debug(f"Received {len(resultset)} rows from {self.connection}")
+        # TODO: fix this part, is messed up again
+        return resultset
