b88bb3533f544157df845456d9baafe3af34f3cb
Srevin Saju <srevinsaju@sugarlabs.org>
2021-05-08 22:30:55 +0300

    feat: add create table features for postgresql adapter

--- noticov/backend/postgresql.py
+++ noticov/backend/postgresql.py
@@ -1,16 +1,21 @@
+from typing import List
+
 import psycopg2
 
 from noticov.backend.base import BaseConnection
 from noticov.backend.tables import Table
 from noticov.covidstats.data import CovidData, CovidDataAttr
+from noticov.logging import make_logger
 
 
 class PostgreSQLConnection(BaseConnection):
     connection = "postgresql"
+    logger = make_logger("psql")
 
     def __init__(self, destination: str = None):
         super().__init__()
         self.conn = psycopg2.connect(destination)
+        self.logger.info("Connection to postgresql database successful")
         self.initialize()
 
     def initialize(self):
@@ -28,7 +33,30 @@ class PostgreSQLConnection(BaseConnection):
                 )
 
     def add_data(self, data: CovidData):
-        pass
+        with self.conn.cursor() as cur:
+            for table in Table:
+                cur.execute(
+                    f"CREATE TABLE IF NOT EXISTS {table} ("
+                    f"{CovidDataAttr.ID} INT PRIMARY KEY, "
+                    f"{CovidDataAttr.TIMESTAMP} INT, "
+                    f"{CovidDataAttr.DEATHS} INT, "
+                    f"{CovidDataAttr.TOTAL_CASES} INT, "
+                    f"{CovidDataAttr.DISCHARGED} INT, "
+                    f"{CovidDataAttr.LOCATION} VARCHAR(50), "
+                    f")"
+                )
 
+    def add_multiple_data(self, data_sequence: List[CovidData]):
+        # TODO: Improve the performance by doing only a single request
+        for data in data_sequence:
+            self.add_data(data)
 
+    def get_all_covid_data(self, table: Table, location: str):
+        with self.conn.cursor() as cur:
+            # Check the current balance.
+            cur.execute(f"SELECT "
+                        f"{CovidDataAttr.TOTAL_CASES}, "
+                        f"{CovidDataAttr.DEATHS}, "
+                        f"{CovidDataAttr.DISCHARGED} FROM {table} "
+                        f"WHERE {CovidDataAttr.LOCATION} = \"{location}\"")
 
